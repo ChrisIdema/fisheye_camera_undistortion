38f671eff3847b096dbebcfc7b17b86f87517d9e
Chris Idema <>
2021-03-19 18:26:09 +0100

    -added transformation to linear with angle

--- image_correction.py
+++ image_correction.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-
+import time
 
 def correct(img_in, k, d, dims):
     dim1 = img_in.shape[:2][::-1]
@@ -11,9 +11,51 @@ def correct(img_in, k, d, dims):
     nk[1,1]=k[1,1]/2
     
     map1, map2 = cv2.fisheye.initUndistortRectifyMap(k, d, np.eye(3), nk, dims, cv2.CV_16SC2)
+    
+    # print(map1.shape)
+    # print(map2.shape)
+    
+    # print(map1)
+    # print(map2)
+
 
     img_out = cv2.remap(img_in, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     return img_out
+
+def correct_tan(src):
+    
+    h_angle = 120
+    w = 640
+    h = 480
+    v_angle = h_angle/640*480
+    s = (h,w,2)
+
+    map1 = np.zeros(s,dtype=np.float32)
+    # cv2.CV_16SC2
+    # map1 = cv2.createmat(h,w,cv2.CV_32FC2)
+    map2= None   
+
+    x_curve = np.linspace(-h_angle/2,h_angle/2,w)
+
+    #ratio = TAN(Radians(angle))/TAN(RADIANS(60))
+    x_curve = np.tan(np.radians(x_curve))/np.tan(np.radians(h_angle/2))
+
+    x_curve = (x_curve+1)/2*(w-1)
+    
+    y_curve = np.linspace(-v_angle/2,v_angle/2,h)
+
+    #ratio = TAN(Radians(angle))/TAN(RADIANS(60))
+    y_curve = np.tan(np.radians(y_curve))/np.tan(np.radians(v_angle/2))
+
+    y_curve = (y_curve+1)/2*(h-1)
+
+    map1[::,::,0] = x_curve
+    # map1[::,::,1] = np.repeat(np.transpose([np.linspace(0,h-1,h)]),repeats=w,axis=1)
+    map1[::,::,1] = np.repeat(np.transpose([y_curve]),repeats=w,axis=1)
+    
+    dst = cv2.remap(src, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    return dst
+
 
 
 if __name__ == '__main__':
@@ -23,5 +65,17 @@ if __name__ == '__main__':
     
     img = cv2.imread('distort.jpg')
     img = correct(img, k=K, d=D, dims=(640,480))
-    cv2.imshow('', img)
     cv2.imwrite('undistorted.jpg', img)
+    
+    img_tan = correct_tan(img)
+    cv2.imwrite('linear.jpg', img_tan)
+    
+    cv2.imshow('', img)     
+    cv2.waitKey(2000)
+    cv2.imshow('', img_tan) 
+    cv2.waitKey(2000)
+    
+    cv2.destroyAllWindows()
+    
+    
+    # img = correct(img, k=K, d=D, dims=(640,480))
