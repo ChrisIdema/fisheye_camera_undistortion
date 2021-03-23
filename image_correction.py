6795c841118ad81b39449f68862dcf556dcec48b
Chris Idema <48433711+ChrisIdema@users.noreply.github.com>
2021-03-23 14:24:14 +0100

    -corrected images

--- image_correction.py
+++ image_correction.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 import time
+import os
 
-def calc_unfish_map(img_in, k, d, dims):
-    dim1 = img_in.shape[:2][::-1]
+def calc_unfish_map(k, d, dims):
+    # dim1 = img_in.shape[:2][::-1]
 
-    # print(dim1)
-    assert dim1[0] / dim1[1] == dims[0] / dims[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+    # # print(dim1)
+    # assert dim1[0] / dim1[1] == dims[0] / dims[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
     
     nk = k.copy()
     nk[0,0]=k[0,0]/2
@@ -32,9 +33,9 @@ def calc_unfish_map(img_in, k, d, dims):
 
 
 
-def calc_tan_map(src,h_angle):    
-    w = src.shape[1]
-    h = src.shape[0]
+def calc_tan_map(dims,h_angle):    
+    w = dims[0]
+    h = dims[1]
     v_angle = h_angle/w*h
     s = (h,w,2)
 
@@ -80,34 +81,44 @@ if __name__ == '__main__':
     K = np.load('./parameters/K.npy')
     D = np.load('./parameters/D.npy')
     
-    img = cv2.imread('distort.jpg')
+    unfishmap1,unfishmap2=calc_unfish_map(k=K, d=D, dims=Dims)
+    tanmap1,tanmap2 = calc_tan_map(Dims,125)
 
-    map1,map2=calc_unfish_map(img, k=K, d=D, dims=Dims)
+    path = r'C:\Users\Chris\Documents\ee\git\hall_stills\wide'
+    dirs = os.listdir(path)
+    image_list = [x for x in dirs if os.path.isfile(os.path.join(path, x))]
+    gray = None
+    for image in image_list:
+        img = cv2.imread(os.path.join(path, image))
+        img = cv2.remap(img, unfishmap1, unfishmap2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT) 
+        cv2.imwrite(os.path.join(path,'corrected',image), img)
+        img = cv2.remap(img, tanmap1, tanmap2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)   
+        cv2.imwrite(os.path.join(path,'linear',image), img)
 
-    start = time.time() 
-    img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)  
-    end = time.time() 
-    print("% s seconds" % (end-start)) 
 
-    cv2.imwrite('undistorted.jpg', img)
-    
-    map1,map2 = calc_tan_map(img,125)
+    # start = time.time() 
+    # img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)  
+    # end = time.time() 
+    # print("% s seconds" % (end-start)) 
 
-    start = time.time() 
-    img_tan = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)   
-    end = time.time() 
-    print("% s seconds" % (end-start)) 
+    # cv2.imwrite('undistorted.jpg', img)
     
-    
-    cv2.imshow('', img)     
-    cv2.waitKey(2000)
-    cv2.imshow('', img_tan) 
-    cv2.waitKey(2000)
+    # map1,map2 = calc_tan_map(img,125)
 
-    cv2.imwrite('undistorted.jpg', img)
-    cv2.imwrite('linear.jpg', img_tan)
+    # start = time.time() 
+    # img_tan = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)   
+    # end = time.time() 
+    # print("% s seconds" % (end-start)) 
     
-    cv2.destroyAllWindows()
     
+    # cv2.imshow('', img)     
+    # cv2.waitKey(2000)
+    # cv2.imshow('', img_tan) 
+    # cv2.waitKey(2000)
+
+    # cv2.imwrite('undistorted.jpg', img)
+    # cv2.imwrite('linear.jpg', img_tan)
     
-    # img = correct(img, k=K, d=D, dims=(640,480))
+    # cv2.destroyAllWindows()
+    
+
